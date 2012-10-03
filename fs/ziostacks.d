f53a4df3901a0a6f9ca9fe8bc6cdd11640108558
Brendan Gregg <brendan.gregg@joyent.com>
2012-10-03 14:18:40 -0700

    scripts after zfsday

---
+++ fs/ziostacks.d
@@ -0,0 +1,36 @@
+#!/usr/sbin/dtrace -s
+/*
+ * ziostacks.d	Show kernel stacks for ZFS zio_read/writes.
+ *
+ * SEE ALSO: iostacks.d, ziocreatestacks.d
+ *
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at http://smartos.org/CDDL
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file.
+ *
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ *
+ * Copyright (c) 2012 Joyent Inc., All rights reserved.
+ * Copyright (c) 2012 Brendan Gregg, All rights reserved.
+ */
+
+fbt::zio_read:entry,
+fbt::zio_write:entry
+{
+	@[probefunc, stack()] = count();
+}
