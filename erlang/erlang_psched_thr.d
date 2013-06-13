dbdbedb56eb28d45d9b2019bf53808198bf1d785
Brendan Gregg <brendan.gregg@joyent.com>
2013-06-13 02:22:35 -0700

    more Erlang

---
+++ erlang/erlang_psched_thr.d
@@ -0,0 +1,26 @@
+#!/usr/sbin/dtrace -s
+/*
+ * erlang_psched_thr.d	Erlang thread scheduled times.
+ * 
+ * This is based on thread-local variables for efficiency;
+ * I checked it vs using an associative array keyed on the
+ * Erlang PID, and there wasn't a difference.
+ */
+
+erlang*:::process-scheduled
+{
+	self->ts = timestamp;
+}
+
+erlang*:::process-unscheduled
+/self->ts/
+{
+	@["ns"] = quantize(timestamp - self->ts);
+	self->ts = 0;
+}
+
+tick-1s
+{
+	printa(@);
+	trunc(@);
+}
