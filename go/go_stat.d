d4f99fe27571507080dda81d2b5173ac6be36c59
Brendan Gregg <brendan.gregg@joyent.com>
2013-11-14 15:51:51 -0800

    reduce latency further

--- go/go_stat.d
+++ go/go_stat.d
@@ -7,8 +7,8 @@
  * COLUMNS:
  * 
  * 	main		main package functions
- * 	os		os package functions
  * 	syscalls	system calls
+ * 	TU%CPU		total percent CPU in user-mode
  *
  * NOTE: the
  * If an interval is not provided, this defaults to 1 second.
@@ -21,20 +21,20 @@ dtrace:::BEGIN
 {
 	interval = $1 ? $1 : 1;
 	sec = 0;
-	printf("%-20s %10s %10s %10s\n", "TIME", "main/s", "os/s",
-	    "syscalls/s");
-	main = 0; os = 0; syscalls = 0;
+	printf("%-20s %10s %10s %10s\n", "TIME", "main/s", "syscalls/s",
+	    "TU%CPU");
+	main = 0; syscalls = 0; oncpu = 0;
 }
 
-pid$target:a.out:main.*:entry    { main++; }
-pid$target:a.out:os.*:entry      { os++; }
-syscall:::entry /pid == $target/ { syscalls++; }
+pid$target:a.out:main.*:entry        { main++; }
+syscall:::entry /pid == $target/     { syscalls++; }
+profile-100 /arg1 && pid == $target/ { oncpu++; }
 
 profile:::tick-1s
 /interval && ++sec == interval/
 {
 	sec = 0;
 	printf("%-20Y %10d %10d %10d\n", walltimestamp,
-	    main / interval, os / interval, syscalls / interval);
-	main = 0; os = 0; syscalls = 0;
+	    main / interval, syscalls / interval, oncpu / interval);
+	main = 0; syscalls = 0; oncpu = 0;
 }
